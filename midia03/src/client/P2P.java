d74e6d8cec8d097b7e1d60072e7ee7061a8c1c53
tiagobencardino@gmail.com <tiagobencardino@gmail.com@e78d06dc-9f73-b792-2003-1822e50659dd>
2011-11-07 02:40:43 +0000

    alteracoes e varios TODO

--- midia03/src/client/P2P.java
+++ midia03/src/client/P2P.java
@@ -1,4 +1,5 @@
 package client;
+import java.io.IOException;
 import java.net.ServerSocket;
 
 import video.Client;
@@ -20,31 +21,53 @@ public class P2P extends Thread implements PeerListener{
 		this.receiveVideoPort = receiveVideoPort;
 		this.sendVideoPort = sendVideoPort;
 	}
+	public void requestVideo(){ //request TO SEND a video
+		//if ok, call sendVideo()
+	}
+	public void acceptVideo(){ //accept TO RECEIVE a video
+		//if ok, call receiveVideo().. que nao tem a porra da chamada do start, tem que modificar video.cliente
+	}
 	public void receiveVideo(){
-		try {
-			video.Client client = new video.Client();
-			client.setIp(ip);
-			client.setPort(sendVideoPort);
-			client.start();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		new Thread ("Receive video thread") {
+			public void run() {
+				video.Client client = new video.Client();
+				client.setIp(ip);
+				client.setPort(sendVideoPort);
+				try {
+					client.start(); //nao pode ser start, tem que ser receive
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}.start();
 
 	}
 	public void sendVideo() throws Exception{
-		int sessionID = 123456;
-		ServerSocket generalSock = new ServerSocket(receiveVideoPort);
-        System.out.println("Esperando por cliente...");
-        Server server = new Server();
-        server.RTSP_ID = sessionID++;
-        
-       // server.serverSocket = generalSock;
-        server.RTSPsocket = generalSock.accept();
-        
-        System.out.println("Usuário conectado ao socket");
-        Thread t = new Thread (server);
-        t.start();
+		new Thread ("send video thread") {
+			public void run() {
+				int sessionID = 123456;
+				ServerSocket generalSock;
+				try {
+					generalSock = new ServerSocket(receiveVideoPort);
+		        System.out.println("Esperando por cliente...");
+		        Server server = new Server();
+		        server.RTSP_ID = sessionID++;
+		        
+		       // server.serverSocket = generalSock;
+				server.RTSPsocket = generalSock.accept();
+
+		        System.out.println("Usuário conectado ao socket");
+		        Thread t = new Thread (server);
+		        t.start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        
+			}
+		}.start();
+			
 
 	}
 	@Override
