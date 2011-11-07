f39203959dee6d7942bcfe588fb9c6128507a763
tiagobencardino@gmail.com <tiagobencardino@gmail.com@e78d06dc-9f73-b792-2003-1822e50659dd>
2011-11-07 02:40:43 +0000

    alteracoes e varios TODO

--- trunk/midia03/src/client/Main.java
+++ trunk/midia03/src/client/Main.java
@@ -11,8 +11,8 @@ public class Main {
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws Exception{
-		//pedro();
-		tiago();
+		pedro();
+	//	tiago();
 //		Thread.sleep(1000);
 //		tiago.call(pedro.getUserName());
 		
@@ -24,6 +24,9 @@ public class Main {
 		tiagop2p.gotP2P("localhost", 5052,5054);
 		System.out.println("fui!");
 		tiagop2p.receiveVideo();
+		System.out.println("receiving");
+		tiagop2p.sendVideo();
+		
 	}
 	public static void pedro() throws Exception{
 		Client pedro = createClient("pedro", "123");
@@ -31,6 +34,9 @@ public class Main {
 		pedrop2p.start();
 		pedrop2p.gotP2P("localhost", 5054,5052);
 		pedrop2p.sendVideo();
+		//Thread.sleep(20000);
+		pedrop2p.receiveVideo();
+		
 	}
 	public static Client createClient(String name, String password) throws IOException{
 		Client client = new Client();
